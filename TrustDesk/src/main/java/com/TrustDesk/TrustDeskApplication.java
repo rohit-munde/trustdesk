b4176f1904ac835a6a48539ae56428933e837d68
Rohit Munde <rohitvmunde@gmail.com>
2026-09-22 23:07:12 +0530

    Initial Setup

---
+++ TrustDesk/src/main/java/com/TrustDesk/TrustDeskApplication.java
@@ -0,0 +1,13 @@
+package com.TrustDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustDeskApplication.class, args);
+	}
+
+}
